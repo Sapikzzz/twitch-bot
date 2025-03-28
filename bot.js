1e5643566ce41836d3fbce57685e3b91f072f291
Sapikzzz <2002jakubs@gmail.com>
2025-03-28 22:55:08 +0100

    Logging messages

--- bot.js
+++ bot.js
@@ -1,18 +1,33 @@
-require('dotenv').config()
+require('dotenv').config();
+const sprintf = require('sprintf-js').sprintf;
+
+function getTimeOfMessage(){
+	const date = new Date();
+	let day = (date.getDate() < 10 ? '0' : '') + date.getDate();
+	let month = date.getMonth() + 1;
+	month = (month < 10 ? '0' : '') + month;
+	let year = date.getFullYear();
+	let hour = (date.getHours() < 10 ? '0' : '') + date.getHours();
+	let minute = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
+	let seconds = (date.getSeconds() < 10 ? '0' : '') + date.getSeconds();
+	
+	return `${day}-${month}-${year} ${hour}:${minute}:${seconds}`
+}
 
 const tmi = require('tmi.js');
 const client = new tmi.Client({
-	options: { debug: true },
+	
 	identity: {
 		username: 'uwuskochany',
 		password: `oauth:${process.env.TOKEN}`
 	},
-	channels: [ 'sapik_' ]
+	channels: [ 'h2p_gucio' ]
 });
+
 client.connect().catch(console.error);
-client.on('message', (channel, tags, message, self) => {
+
+client.on('message', (channel, userstate, message, self) => {
 	if(self) return;
-	if(message.toLowerCase() === '!hello') {
-		client.say(channel, `@${tags.username}, heya!`);
-	}
+	let timeOfMessage = getTimeOfMessage();
+	console.log(`[${timeOfMessage}] ${userstate['display-name']}: ${message}`)
 });
